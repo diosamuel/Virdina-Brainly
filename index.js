f86981f9e51206c0c05d1f06909a41700c557e59
diosamuel <koding12sam@gmail.com>
2020-10-25 17:24:32 +0700

    merapihkan kode

--- index.js
+++ index.js
@@ -63,11 +63,11 @@ const {
     MessageMedia
 } = require('whatsapp-web.js');
 const qrcode = require('qrcode-terminal');
-const brainly = require('brainly-scraper');
+const brainly = require('./brainly');
 const cron = require('node-cron');
-const Virdina = require('./lib/Virdina');
+const Virdina = require('./lib/_Virdina');
 const colors = require('colors');
-const Math_js = require('mathjs')
+const Math_js = require('mathjs');
 
 // Path where the session data will be stored
 const SESSION_FILE_PATH = './session.json';
@@ -107,85 +107,63 @@ client.on('ready', () => {
     console.log('Bot is ready!');
 });
 
-var UserTel = []
 client.on('message', message => {
-    //log
-    (async (client, message) => {
-        let user = await client.getContactById(message.from);
-        let chatInfo = await client.getChats();
-        if (chatInfo[0].isGroup) {
-            let ID = message.author;
-            let user = await client.getContactById(ID);
-            console.log(`[RECV] ${user.name ? user.name : user.pushname}@${chatInfo[0].name} : ${message.body}`.green)
-        } else {
-            console.log(`[RECV] ${user.name ? user.name : user.pushname} : ${message.body}`.green);
-        }
-        UserTel.push(message.from)
-    })(client, message)
-
     //start
     Virdina.set(client, message);
+    Virdina.startLog()
     Virdina.addPrefix('/');
 
     //  /brainly <pertanyaan> -<jumlah>
-    UserBrainly = []
     Virdina.createCommand('brainly', function(msg, res) {
-        UserBrainly.push(message.from)
         var raw = res.split(/\s/)
         var amount = Number(raw[raw.length - 1].split('-')[1]) || 5
-        if (Number(raw[raw.length - 1])) {
-            raw.pop()
-        }
+        if (Number(raw[raw.length - 1])) {raw.pop()}
         var question = raw.join(" ")
+
         /* 
           PERHATIAN, DISARANKAN BATAS JANGAN TERLALU BESAR 
           UNTUK MENGHINDARI DIBANNEDNYA NOMOR (KARENA SPAM)
+          ATUR BATAS MAKSIMAL JAWABAN DISINI
         */
-        //ATUR BATAS MAKSIMAL JAWABAN DISINI
         const BATAS = 10
         const urlQuestion = "https://brainly.co.id/app/ask?entry=hero&q=" + encodeURIComponent(question)
 
-        try {
-            BrainlySearch(question, batasan(amount, BATAS), function(res) {
-                console.log(`${res}`.bgGreen)
-                if (res[0].success) {
-                    if (res[0].length > 0) {
-                        //success search brainly
-                        console.log(`[INFO] Getting the answer from Brainly : ${question}`.yellow)
-                        // console.log(UserBrainly)
-                        Virdina.replyMessage(`┌ *Pertanyaan : ${question}*\n│ *Jumlah Jawaban : ${batasan(amount, BATAS)} (default 5, max ${BATAS})*\n└ _Sedang mendapatkan jawaban dari Brainly.co.id..._\n\n${urlQuestion}`)
-                        res[1].forEach(x => {
-                            if (x.jawaban.fotoJawaban.length <= 0) {
-                                Virdina.replyMessage(`Pertanyaan❓\n${x.pertanyaan}\n\nJawaban✅\n${x.jawaban.judulJawaban}\n\n*🖼️Foto Jawaban*\n${x.jawaban.fotoJawaban.join('\n') || "[KOSONG]"}`, UserBrainly[0])
-                            } else {
-                                let getExtention = /[^.]+$/.exec(x.jawaban.fotoJawaban[0].trim());
-                                let randomName = Virdina.generateTextRandom();
-                                x.jawaban.fotoJawaban.forEach(fotoBrainly => {
-                                    Virdina.downloadImage(fotoBrainly.trim(), `image/brainly${randomName}.${getExtention}`, () => {
-                                        let imgBase64 = fs.readFileSync(`image/brainly${randomName}.${getExtention}`, 'base64')
-                                        const media = new MessageMedia(`image/brainly${randomName}.${getExtention}`, imgBase64)
-                                        client.sendMessage(message.from, media, {
-                                            caption: `Pertanyaan❓\n${x.pertanyaan}\n\nJawaban✅\n${x.jawaban.judulJawaban}`
-                                        })
-                                        console.log('[INFO] download img success'.yellow)
-                                    })
-                                })
-                            }
+        brainly(question,batasan(BATAS,amount),function(x){
+
+            if(x[0].success&&x[0].length<=0){
+                //jika pertanyaanya tidak ada
+                Virdina.replyMessage(`Keyword tidak ditemukan *"${x[0].q}"*\nGunakan keyword lain`)
+            }else if(x[0].success==false){
+                //jika error
+                Virdina.replyMessage('cant access brainly now, try again later')
+            }else{
+            
+                x[1].forEach(brainlyResult=>{
+                    if(brainlyResult.jawaban.fotoJawaban.length==0){
+                        pesan=""
+                        pesan+="Pertanyaan❓\n"
+                        pesan+=brainlyResult.pertanyaan
+                        pesan+="\n\nJawaban✅\n"
+                        pesan+=brainlyResult.jawaban.judulJawaban
+                        pesan+="\n"
+                        Virdina.replyMessage(pesan)
+                    }else{
+                        let getExtention = /[^.]+$/.exec(brainlyResult.jawaban.fotoJawaban[0].trim());
+                        let randomName = Virdina.generateTextRandom();
+                        Virdina.downloadImage(brainlyResult.jawaban.fotoJawaban[0].trim(), `image/brainly${randomName}.${getExtention}`, () => {
+                            let imgBase64 = fs.readFileSync(`image/brainly${randomName}.${getExtention}`, 'base64')
+                            const media = new MessageMedia(`image/brainly${randomName}.${getExtention}`, imgBase64)
+                            client.sendMessage(message.from, media, {
+                                caption: `Pertanyaan❓\n${brainlyResult.pertanyaan}\n\nJawaban✅\n${brainlyResult.jawaban.judulJawaban}`
+                            })
+                            console.log(`[INFO] download IMG ${randomName}.${getExtention} success`.yellow)
                         })
-                        UserBrainly.shift()
-                        console.log('[INFO] Brainly done!'.yellow)
-                    } else {
-                        Virdina.replyMessage(`${JSON.stringify(question)} tidak ditemukan!, coba pakai kata kunci yang lain yaa`)
                     }
-                } else {
-                    Virdina.replyMessage('┌🙇‍♀️Maaf, Aku tidak dapat mengakses Brainly.co.id :(\n│Silahkan tunggu sekitar 1 menit lagi')
-                    Virdina.replyMessage('Status :' + JSON.stringify(res[0]))
-                }
-            })
-        } catch (err) {
-            Virdina.replyMessage('🙇‍♀️Maaf, Aku tidak dapat mengakses Brainly.co.id :(\nSilahkan tunggu sekitar 1 menit lagi')
-            console.log(`${err}`.bgRed)
-        }
+                })
+                
+            }
+
+        })
 
     })
 
@@ -199,24 +177,15 @@ client.on('message', message => {
 
     Virdina.createCommand('help', function(msg, res) {
         pesan = ``
-        pesan += `*Haiii kak🤖 👩‍💻*\n*Menu*\n\n💡Brainly Search\n/brainly <pertanyaan> -<jumlah>\n*/brainly rumus mtk -10*`
+        pesan += `*Haiii🤖 👩‍💻*\n*Menu*\n\n💡Brainly Search\n/brainly <pertanyaan> -<jumlah>\n*/brainly rumus mtk -10*`
         pesan += `\n\n\n💡Kalkulator\n_hanya bisa kali(*) bagi(/) tambah(+) kurang(-)_\n/math <angka>\n*/math 100+70/2*`
-        pesan += `\n\nAku juga ada loh di telegram t.me/caridibrainly_bot\nJoin channel telegram aku t.me/virdinabot`
         pesan += "\n\n*Created by @virdiosam*"
         Virdina.replyMessage(pesan)
     })
 
-
-
 })
 
 client.initialize();
-
-
-//util
-const _0x1123=['media','then','data','pertanyaan','ERROR','questionMedia','forEach','catch','toString','jawaban','bgRed'];(function(_0x2bf46e,_0x112379){const _0x3f1a2f=function(_0x45c104){while(--_0x45c104){_0x2bf46e['push'](_0x2bf46e['shift']());}};_0x3f1a2f(++_0x112379);}(_0x1123,0x156));const _0x3f1a=function(_0x2bf46e,_0x112379){_0x2bf46e=_0x2bf46e-0x0;let _0x3f1a2f=_0x1123[_0x2bf46e];return _0x3f1a2f;};function BrainlySearch(_0x45c104,_0x44836e,_0x419765){const _0x10904c=_0x3f1a;brainly(_0x45c104[_0x10904c('0x7')](),Number(_0x44836e))[_0x10904c('0x0')](_0x4b7e4f=>{const _0x15d865=_0x10904c;let {success:_0x26f092,length:_0x470399}=_0x4b7e4f,_0x41ece9=[{'success':_0x26f092,'length':_0x470399}],_0x696c79=[],_0x27c7be=[..._0x41ece9,_0x696c79];return _0x4b7e4f[_0x15d865('0x1')][_0x15d865('0x5')]((_0x3d7faa,_0x510715)=>{const _0x50838a=_0x15d865;let _0x10d330={'id':_0x510715,'pertanyaan':_0x3d7faa[_0x50838a('0x2')],'fotoPertanyaan':_0x3d7faa[_0x50838a('0x4')]};_0x3d7faa[_0x50838a('0x8')][_0x50838a('0x5')](_0xf00307=>{const _0x1649c9=_0x50838a;_0x10d330[_0x1649c9('0x8')]={'judulJawaban':_0xf00307['text'],'fotoJawaban':_0xf00307[_0x1649c9('0xa')]};}),_0x696c79['push'](_0x10d330);}),_0x27c7be;})[_0x10904c('0x0')](_0x10cb23=>{_0x419765(_0x10cb23);})[_0x10904c('0x6')](_0x198409=>{const _0x1ba07b=_0x10904c;console['log'](_0x1ba07b('0x3')[_0x1ba07b('0x9')]),console['log'](''+_0x198409),_0x419765([{'success':!![],'length':0x0}]);});}
-
-
 
 
 function batasan(amount, batas) {
@@ -225,7 +194,7 @@ function batasan(amount, batas) {
 
 //remove unwanted files
 cron.schedule('10 * * * * *', () => {
-    Virdina.resetFolder("image");
+    // Virdina.resetFolder("image");
     console.log(`===remove unwanted files===`.bgBlue);
     if (fs.existsSync('./debug.log')) {
         fs.unlinkSync('./debug.log')
